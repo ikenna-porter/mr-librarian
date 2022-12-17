418910bba978004c76d13137e1debda1634c9b2e
Ikenna <ikenna.porter@gmail.com>
2022-12-17 13:25:43 -0300

    build: create project setup

---
+++ src/api_manager.py
@@ -0,0 +1,14 @@
+import requests
+import json
+ 
+class APIManager():
+    """A class responsible for fetching, decoding and changing the shape of data received from Google Books API"""
+    def fetch():
+        """A method that performs the fetch request"""
+        pass
+
+    def decode():
+        pass
+
+    def clean_up_data():
+        pass
---
+++ tests/test_api_manager.py
@@ -0,0 +1,8 @@
+import unittest
+from src.api_manager import APIManager
+
+class TestAPIManager(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
