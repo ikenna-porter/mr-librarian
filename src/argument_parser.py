10989e188930b556e3c893b8571b18f6e77add69
Ikenna <ikenna.porter@gmail.com>
2022-12-19 15:42:37 -0300

    build: Changed command line argument input structure to accept sub-arguments for find-book command

--- src/argument_parser.py
+++ src/argument_parser.py
@@ -5,27 +5,32 @@ class ArgumentParser():
     """A class that accepts user's input, parses it, and decides what course of action to take based on input."""
 
     def argument_parser_configs():
-        # """Establishes what arguments are acceptable for the application."""
-        # parser = argparse.ArgumentParser(description="Welcome to Mr. Librarian. Let's get reading!")
-        # parser.add_argument("--find-book", help="Find a book from Google Books")
-        # parser.add_argument("--add-book", choices=["1","2","3","4","5"], help="Add a book to your reading list")
-        # parser.add_argument("--view-list", action="store_true", help="View your reading list")
-        # return  parser.parse_args()
-
+        #create the top-level parser
         parser = argparse.ArgumentParser(description="Welcome to Mr. Librarian. Let's get reading!")
-        subparser = parser.add_argument("--find-book", help="Find a book from Google Books")
-        subparser.add_argument
-        parser.add_argument("--add-book", choices=["1","2","3","4","5"], help="Add a book to your reading list")
-        parser.add_argument("--view-list", action="store_true", help="View your reading list")
+        subparsers = parser.add_subparsers()
+
+        #establishes format for arguments to find a book
+        #python main.py find-book -f <user input>
+        find_book_parser = subparsers.add_parser("find-book", help="Finds books using Google Books API")
+        find_book_parser.add_argument("-f", type=str, nargs="+")
+
+        #establishes format for arguments to find a book
+        #python main.py book-list --add 2
+        #python main.py book-list --view
+        book_list_parser = subparsers.add_parser("book-list", help="Adds book to reading list")
+        book_list_parser.add_argument("--add", choices=["1","2","3","4","5"])
+        book_list_parser.add_argument("--view", action="store_true", help="View your reading list")
+
         return  parser.parse_args()
     
     def parse_arguments(self, user_input):
         """Takes in user's input and decides what course of action the program should take."""
         args_dict = vars(user_input)
+        print(args_dict)
         for arg in args_dict:
             print(arg)
             if args_dict[arg]:
-                if arg == "find_book":
+                if arg == "f":
                     self.parse_find_books_arg(arg)
 
                     books = APIManager()
