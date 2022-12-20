94d0a55c3677d6b556f90b010a643e2a5ece01f4
Ikenna <ikenna.porter@gmail.com>
2022-12-20 12:54:52 -0300

    fix: Fix bug where add functionality would not query database to search for existing tables

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
-from src.prompts import PromptManager
-from src.api_manager import APIManager
 from src.reading_list import ReadingList
 from src.argument_parser import ArgumentParser
+from tabulate import tabulate
 import queries
 
-
 def main():
-    prompt = PromptManager()
-    prompt.print_welcome_text()
-
-
     user_input = ArgumentParser.argument_parser_configs()
 
-    #parses user input to determine what command to execute
-    arguments = ArgumentParser()
-    response = arguments.parse_arguments(user_input)
+    # Parses user input for command line argument and determines what command to execute based on provided argument
+    arguments_parser = ArgumentParser()
+    argument = arguments_parser.parse_arguments(user_input)
 
-    #if user chose to find a book from API
-    if response[0] == "f":
-        prompt.display_fetched_books(response[1])
+    # If user entered 'find-book -f' argument (to search for a book from Google Books API):
+    if argument[0] == "f":
+
+        print("\n", tabulate(argument[1], headers="keys", tablefmt="simple"), "\n") #displays fetched books
 
         if queries.does_fetched_books_table_exist() == 0:
-            print(queries.does_fetched_books_table_exist())
-            queries.create_and_insert_in_recently_fetched_table(response[1])
+            queries.create_and_insert_in_recently_fetched_table(argument[1])
         else:
-            print(queries.does_fetched_books_table_exist())
             queries.delete_recently_fetched_books()
-            queries.create_and_insert_in_recently_fetched_table(response[1])
+            queries.create_and_insert_in_recently_fetched_table(argument[1])
 
-    #if user chose to add a book to reading list
-    elif response[0] == "a":
+    # If user entered 'reading-list -a' argument (to add a book to reading list):
+    elif argument[0] == "a":
         list_ = ReadingList()
-        list_.add_book(response[1])
+        list_.add_book(argument[1])
     
-    #if user chose to view reading list
-    elif response[0] == "v": 
+    # If user entered 'reading-list -v' argument (to view reading list):
+    elif argument[0] == "v": 
         list_ = ReadingList()
         list_.view_list()
 
--- src/reading_list.py
+++ src/reading_list.py
@@ -1,26 +1,30 @@
 import queries
 from sqlite3 import OperationalError
+from tabulate import tabulate
 class ReadingList():
-    """A list of books the user wants to read"""
+    """A class that represents a list of books the user wants to read."""
 
     def add_book(self, num):
-        """A class that adds books to the reading list"""
-        # try:
-        if queries.does_reading_list_table_exist() == 1:
-            book = queries.select_book_to_add(num)
-            queries.add_book_to_reading_list(book[0])
-            self.view_list()
-        else:
-            print(queries.does_reading_list_table_exist())
-            queries.create_reading_list_table()
-            book = queries.select_book_to_add(num)
-            queries.add_book_to_reading_list(book[0])
-            self.view_list()
-        # except OperationalError as e:
-        #     print("Silly, you can't add a nonexistent book to your reading list. Run 'python main.py find-book -f <search query>' first")
-        #     print(e)
+        """Adds books to the reading list."""
+        try:
+            if queries.does_reading_list_table_exist() == 1:
+                book = queries.select_book_to_add(num)
+                queries.add_book_to_reading_list(book[0])
+                print("\n*Book successfully added*\n")
+                self.view_list()
+            else:
+                print(queries.does_reading_list_table_exist())
+                queries.create_reading_list_table()
+                book = queries.select_book_to_add(num)
+                queries.add_book_to_reading_list(book[0])
+                print("\n*Book successfully added*\n")
+                self.view_list()
+        except OperationalError as e:
+            print("Silly, you can't add a nonexistent book to your reading list. Run 'python main.py find-book -f <search query>' first.")
+            print(e)
 
     def view_list(self):
         """Displays the user's reading list in the console."""
-        list = queries.show_reading_list()
-        print(list)
+        list = queries.select_reading_list()
+        print("\nYOUR READING LIST:\n")
+        print(tabulate(list, headers=["Title", "Author(s)", "Publisher"], tablefmt="simple"), "\n")
